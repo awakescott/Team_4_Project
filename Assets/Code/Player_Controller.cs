5a3c351e219f8eeea94df30d7616cea4a4762755
awakescott <53873956+awakescott@users.noreply.github.com>
2020-03-03 10:58:42 -0500

    Player_controller_Complete
    the player controller for the prototype works. Will experiment with it later.

---
+++ Assets/Code/Player_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    private float PlayerSpeed = 3;
+
+    private Rigidbody2D PlayerBody;
+    private Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        PlayerBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput * PlayerSpeed;
+    }
+
+    void FixedUpdate()
+    {
+
+        PlayerBody.MovePosition(PlayerBody.position + moveVelocity * Time.fixedDeltaTime);
+
+
+    }
+}
