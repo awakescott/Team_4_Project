a1c6084a04f51e8cb57dabcd6e89462346c9ecb7
awakescott <53873956+awakescott@users.noreply.github.com>
2020-03-03 13:31:54 -0500

    orange bullet
    orange bullet

--- Assets/Code/BulletPoint.cs
+++ Assets/Code/BulletPoint.cs
@@ -19,6 +19,4 @@ public class BulletPoint : MonoBehaviour
         
     }
 
-
-
 }
---
+++ Assets/Code/BulletPoint2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPoint2 : MonoBehaviour
+{
+
+    public GameObject Bullet2;
+    // Update is called once per frame
+    void Update()
+    {
+
+            if (Input.GetButtonDown("Fire2"))
+            {
+
+                Instantiate(Bullet2, transform.position, transform.rotation);
+
+            }
+
+        
+    }
+}
