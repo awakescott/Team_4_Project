3e4923a835c255823183c32e85ceef95b0962ea2
awakescott <53873956+awakescott@users.noreply.github.com>
2020-03-04 11:41:06 -0500

    Revert "Merge branch 'UI-Menus'"
    This reverts commit 0b8c00fe89dfdbabccaba98f9dd36531c7f824fa, reversing
    changes made to 2796877d26fd5fdadedd3971e6dec9cd897f3e54.

--- Assets/Scripts/DestroyPlayer.cs
+++ Assets/Scripts/DestroyPlayer.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class DestroyPlayer : MonoBehaviour
 {
-
-    public GameObject gameManager;
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
             Destroy(gameObject);
-            gameManager.SendMessage("OnPlayerDied");
         }
     }
 }
