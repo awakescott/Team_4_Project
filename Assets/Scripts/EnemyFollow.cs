9760d2a740d1a4eb5147bc9ca96683cbceca0972
OverlordVGS <57149179+OverlordVGS@users.noreply.github.com>
2020-03-04 16:15:22 -0500

    UI Controller Compatibility and New Enemy Typer
    Controller and Arcade works on UI, and new Carrier form enemy is added. Spawns seeker enemies that track player.

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float followSpeed = 1f;
+    Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        SetTarget();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            SetTarget();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
+    }
+
+    void SetTarget()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position, followSpeed * Time.deltaTime);
+    }
+}
