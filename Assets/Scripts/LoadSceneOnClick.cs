28e07237aec6a15cb50a455715a69cdec0b6d27e
OverlordVGS <57149179+OverlordVGS@users.noreply.github.com>
2020-03-04 11:20:28 -0500

    UI Menus
    Created Main Menu, Level, How to Play and Game Over connections thrught scene indexes. Now all buttons work and the game is connceted.

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    // When "Start Game" is selected, the game scene loads
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
