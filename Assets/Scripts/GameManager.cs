fc4f4eb32e79165601d2ce5f430672c669b69328
OverlordVGS <57149179+OverlordVGS@users.noreply.github.com>
2020-03-04 11:55:17 -0500

    Revert "Revert "Merge branch 'UI-Menus'""
    This reverts commit 3e4923a835c255823183c32e85ceef95b0962ea2.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Set Variables
+    public float gameOverDelay;
+
+    // When the player is killed, wait to call the Game Over scene
+    void OnPlayerDied()
+    {
+        Invoke("GameOver", gameOverDelay);
+    }
+
+    // When the game is over, the game over screen shows up
+    void GameOver()
+    {
+        SceneManager.LoadScene("Game Over Screen");
+    }
+}
