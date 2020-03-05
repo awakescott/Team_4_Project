40d3366fceefdfd26929000a4ac0c03012cc2fb3
awakescott <53873956+awakescott@users.noreply.github.com>
2020-03-05 11:40:02 -0500

    added more art

--- Assets/Code/BulletMovement.cs
+++ Assets/Code/BulletMovement.cs
@@ -6,13 +6,13 @@ public class BulletMovement : MonoBehaviour
 {
   
 
-    private int BulletSpeed = 7;
+    private int BulletSpeed = 5;
     // Update is called once per frame
     void Update()
     {
 
         transform.Translate(Vector2.right * BulletSpeed * Time.deltaTime);
-        Destroy(gameObject, 0.7f);
+        Destroy(gameObject, 0.5f);
 
     }
 }
